69dd10f3c8d3842e493a2dfc4188f7a0ebcaab5c
ashiq331 <ashiq33030@gmail.com>
2024-03-08 14:58:07 +0600

    some issu fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,7 @@ const addEventOnElem = function (elem, type, callback) {
   }
 };
 
-// Sticky Header
 
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", function () {
-  header.classList.toggle("scrolled", window.scrollY > 13);
-  console.log(this.window.scrollY);
-});
 
 // Navbar Toggle
 
